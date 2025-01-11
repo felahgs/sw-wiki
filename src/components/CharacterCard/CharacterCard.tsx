c84b30f11f0ec60f361ed64ceb1c79b157c45834
Felipe Souza <fgsouza93@gmail.com>
2025-01-11 13:25:26 -0300

    refactor: character card height

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -12,7 +12,7 @@ const CharacterCard: React.FC<CharacterCardProps> = ({ name, uid, image }) => {
   return (
     <div
       key={uid}
-      className="bg-gray-800 text-secondary p-4 rounded-md shadow hover:shadow-lg transition-shadow w-full md:w-52 max-h-52"
+      className="bg-gray-800 text-secondary p-4 rounded-md shadow hover:shadow-lg transition-shadow w-full md:w-52 md:h-56"
     >
       <div className="relative w-full grow-0 h-32 mb-4">{image}</div>
       <h2 className="text-xl font-semibold text-center">{name}</h2>
