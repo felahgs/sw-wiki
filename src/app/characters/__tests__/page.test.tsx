05bcbddcad3cdd05e4be548832c8bfacf47ddebd
Felipe Souza <fgsouza93@gmail.com>
2025-01-12 13:16:23 -0300

    test: add fetchCharacter test for the characters page

--- src/app/characters/__tests__/page.test.tsx
+++ src/app/characters/__tests__/page.test.tsx
@@ -1,16 +1,26 @@
 import React from "react";
+import userEvent from "@testing-library/user-event";
 import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import CharactersPage from "../page";
+
 import { CharactersResponse, listCharacters } from "@/services/characters";
 import { characterList } from "@/utils/mocks";
+
+import CharactersPage from "../page";
 
 jest.mock("@/services/characters", () => ({
   listCharacters: jest.fn(),
 }));
 
 jest.mock("../characters_list", () =>
-  jest.fn(() => <div>Mocked Characters List</div>),
+  jest.fn(({ charactersPromise }) => (
+    <div>
+      <p>Mocked Characters List</p>
+      <button onClick={async () => await charactersPromise(2, "search string")}>
+        trigger
+      </button>
+    </div>
+  )),
 );
 
 const mockCharactersResponse: CharactersResponse = {
@@ -20,7 +30,7 @@ const mockCharactersResponse: CharactersResponse = {
 };
 
 describe("CharactersPage", () => {
-  it("renders correctly with default searchParams", async () => {
+  it("should render correctly with default searchParams", async () => {
     (listCharacters as jest.Mock).mockResolvedValueOnce(mockCharactersResponse);
 
     const mockSearchParams = Promise.resolve({
@@ -41,12 +51,33 @@ describe("CharactersPage", () => {
     ).toBeInTheDocument();
   });
 
-  it("renders results message when name is present in searchParams", async () => {
+  it("should render results message when name is present in searchParams", async () => {
     const mockSearchParams = Promise.resolve({ page: undefined, name: "Luke" });
 
     render(await CharactersPage({ searchParams: mockSearchParams }));
 
     expect(await screen.findByText("Showing results for")).toBeInTheDocument();
     expect(await screen.findByText('"Luke"')).toBeInTheDocument();
+  });
+
+  it("should call listCharacters when calling fetchCharacters inside the CharactersList component", async () => {
+    (listCharacters as jest.Mock).mockResolvedValueOnce(mockCharactersResponse);
+
+    const mockSearchParams = Promise.resolve({
+      page: undefined,
+      name: undefined,
+    });
+
+    render(await CharactersPage({ searchParams: mockSearchParams }));
+
+    const triggerButton = screen.getByText("trigger");
+    userEvent.click(triggerButton);
+
+    await waitFor(() => {
+      expect(listCharacters).toHaveBeenCalledWith({
+        page: 2,
+        name: "search string",
+      });
+    });
   });
 });
